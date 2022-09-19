3bda769da1fa196a0e256b2766b9aa6a2256bd57
XT <xt@xorde.co>
2022-09-19 18:08:41 -0400

    Updated VitePress config

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -1,4 +1,12 @@
 export default {
 	lang: 'en-US',
 	title: 'Remitt Identity',
+	themeConfig: {
+		socialLinks: [
+			{ icon: 'github', link: 'https://github.com/remitt-repos/corporate-identity' },
+		],
+		footer: {
+			copyright: 'Copyright © 2022 Remitt Foundation',
+		}
+	}
 }
